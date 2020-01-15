a2487c85ddb03ba83f425f0ce9d3cf6a5164e264
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2020-01-14 18:56:35 -0800

    Added a button to upload an image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: null
+    }
+  }
+
+  selectImage = (event) => {
+    this.setState({img: URL.createObjectURL(event.target.files[0])})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Please select an image
+          </p>
+          <img src={this.state.img}/>
+          <form>
+            <input type="file" onChange={this.selectImage}/>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
