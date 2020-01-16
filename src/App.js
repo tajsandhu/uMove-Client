c442067ba4701b274289bfcddbedc6f81db1203c
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2020-01-15 17:07:30 -0800

    Added aws cognito to the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
 
-export default class App extends React.Component {
+Amplify.configure(awsconfig)
+
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'My custom email label',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+  ]
+};
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      img: null
+      img: null,
+      imgURL: null,
+      height: 500,
+      width: 500,
     }
   }
 
+  
+
   selectImage = (event) => {
-    this.setState({img: URL.createObjectURL(event.target.files[0])})
+    if (event != null) {
+      this.setState({
+        img: event.target.files[0],
+        imgURL: URL.createObjectURL(event.target.files[0])
+      })
+    }
+  }
+
+  uploadSelected = () => {
+    window.alert(this.state.imgURL)
   }
 
   render() {
@@ -20,13 +54,17 @@ export default class App extends React.Component {
           <p>
             Please select an image
           </p>
-          <img src={this.state.img}/>
+          <img src={this.state.imgURL} style={{maxWidth: 750, maxHeight: 500}}/>
           <form>
             <input type="file" onChange={this.selectImage}/>
           </form>
+          <button onClick={this.uploadSelected}>
+            Upload
+          </button>
         </header>
       </div>
     );
   }
 }
 
+export default withAuthenticator(App, true);
